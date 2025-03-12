972abfea119009df94091eb4c0df9c485f0c33a2
InigoLopez1999 <iniloay@gmail.com>
2025-03-12 22:22:23 +0100

    Cambios HLF

--- funciones.py
+++ funciones.py
@@ -5,68 +5,98 @@ def nuevo_tablero (Tamano):
     return tablero
 
 def posicionar_barco (tablero):
+    global Numero_barcos
+    global num_portaaviones
+    global num_fragatas
+    global num_acorazados
+    global num_destructores
     barco_a_colocar = input ("Qué barco desea colocar en el tablero?").lower()
-    coord_x = int(input ("Dígame la coordenada X del barco a colocar:"))
-    coord_y = int(input ("Dígame la coordenada Y del barco a colocar:"))
+    while barco_a_colocar not in ["fragata","destructor","acorazado","portaaviones"]:
+        print("Entrada no válida. Vuelva a intentarlo")
+        barco_a_colocar = input ("Qué barco desea colocar en el tablero?").lower()
 
     # Barco de 1 de eslora
 
     if barco_a_colocar == "fragata":
-        tablero[coord_x][coord_y] = "B"
-        num_fragatas = num_fragatas - 1
+        if barco_a_colocar == "fragata" and num_fragatas == 0:
+            print("Ya no quedan más fragatas")
+            barco_a_colocar = input ("Qué barco desea colocar en el tablero?").lower()
+        while num_fragatas > 0:
+            coord_x = int(input ("Dígame la coordenada X del barco a colocar:"))
+            coord_y = int(input ("Dígame la coordenada Y del barco a colocar:"))
+            tablero[coord_x][coord_y] = "B"
+            num_fragatas = num_fragatas - 1
 
     # Barco de 2 de eslora
 
     elif barco_a_colocar == "destructor":
-        tablero[coord_x][coord_y] = "B"
-        list_eslora = [1]
-        orientacion = input ("Cómo quiere colocar el barco, en horizontal o en vertical?").lower()
-        if orientacion == "horizontal":
-            este_oeste = input ("Hacia el este o hacia el oeste?")
-            if este_oeste == "este":
-                for eslora in list_eslora:
-                    tablero[coord_x][coord_y + eslora] = "B"
-            elif este_oeste == "oeste":
-                for eslora in list_eslora:
-                    tablero[coord_x][coord_y - eslora] = "B"
-        elif orientacion == "vertical":
-            norte_sur = input ("Hacia el norte o hacia el sur?")
-            if norte_sur == "norte":
-                for eslora in list_eslora:
-                    tablero[coord_x - eslora][coord_y] = "B"
-            elif norte_sur == "sur":
-                for eslora in list_eslora:
-                    tablero[coord_x + eslora][coord_y] = "B"
-        num_destructores = num_destructores - 1
+        if barco_a_colocar == "destructor" and num_destructores == 0:
+            print("Ya no quedan más destructores")
+            barco_a_colocar = input ("Qué barco desea colocar en el tablero?").lower()
+        while num_destructores > 0:
+            coord_x = int(input ("Dígame la coordenada X del barco a colocar:"))
+            coord_y = int(input ("Dígame la coordenada Y del barco a colocar:"))
+            tablero[coord_x][coord_y] = "B"
+            list_eslora = [1]
+            orientacion = input ("Cómo quiere colocar el barco, en horizontal o en vertical?").lower()
+            if orientacion == "horizontal":
+                este_oeste = input ("Hacia el este o hacia el oeste?")
+                if este_oeste == "este":
+                    for eslora in list_eslora:
+                        tablero[coord_x][coord_y + eslora] = "B"
+                elif este_oeste == "oeste":
+                    for eslora in list_eslora:
+                        tablero[coord_x][coord_y - eslora] = "B"
+            elif orientacion == "vertical":
+                norte_sur = input ("Hacia el norte o hacia el sur?")
+                if norte_sur == "norte":
+                    for eslora in list_eslora:
+                        tablero[coord_x - eslora][coord_y] = "B"
+                elif norte_sur == "sur":
+                    for eslora in list_eslora:
+                        tablero[coord_x + eslora][coord_y] = "B"
+            num_destructores = num_destructores - 1
         
     # Barco de 3 de eslora
 
     elif barco_a_colocar == "acorazado":
-        tablero[coord_x][coord_y] = "B"
-        list_eslora = [1,2]
-        orientacion = input ("Cómo quiere colocar el barco, en horizontal o en vertical?").lower()
-        if orientacion == "horizontal":
-            este_oeste = input ("Hacia el este o hacia el oeste?")
-            if este_oeste == "este":
-                for eslora in list_eslora:
-                    tablero[coord_x][coord_y + eslora] = "B"
-            elif este_oeste == "oeste":
-                for eslora in list_eslora:
-                    tablero[coord_x][coord_y - eslora] = "B"
-        elif orientacion == "vertical":
-            norte_sur = input ("Hacia el norte o hacia el sur?")
-            if norte_sur == "norte":
-                for eslora in list_eslora:
-                    tablero[coord_x - eslora][coord_y] = "B"
-            elif norte_sur == "sur":
-                for eslora in list_eslora:
-                    tablero[coord_x + eslora][coord_y] = "B"
-        num_acorazados = num_acorazados - 1
+        if barco_a_colocar == "acorazado" and num_acorazados == 0:
+            print("Ya no quedan más acorazados")
+            barco_a_colocar = input ("Qué barco desea colocar en el tablero?").lower()
+        while num_acorazados > 0:
+            coord_x = int(input ("Dígame la coordenada X del barco a colocar:"))
+            coord_y = int(input ("Dígame la coordenada Y del barco a colocar:"))
+            tablero[coord_x][coord_y] = "B"
+            list_eslora = [1,2]
+            orientacion = input ("Cómo quiere colocar el barco, en horizontal o en vertical?").lower()
+            if orientacion == "horizontal":
+                este_oeste = input ("Hacia el este o hacia el oeste?")
+                if este_oeste == "este":
+                    for eslora in list_eslora:
+                        tablero[coord_x][coord_y + eslora] = "B"
+                elif este_oeste == "oeste":
+                    for eslora in list_eslora:
+                        tablero[coord_x][coord_y - eslora] = "B"
+            elif orientacion == "vertical":
+                norte_sur = input ("Hacia el norte o hacia el sur?")
+                if norte_sur == "norte":
+                    for eslora in list_eslora:
+                        tablero[coord_x - eslora][coord_y] = "B"
+                elif norte_sur == "sur":
+                    for eslora in list_eslora:
+                        tablero[coord_x + eslora][coord_y] = "B"
+            num_acorazados = num_acorazados - 1
+        
     
     # Barco de 4 de eslora
 
     elif barco_a_colocar == "portaaviones":
-        while num_portaaviones != 0:
+        if barco_a_colocar == "portaaviones" and num_portaaviones == 0:
+            print("Ya no quedan más portaaviones")
+            barco_a_colocar = input ("Qué barco desea colocar en el tablero?").lower()
+        while num_portaaviones > 0:
+            coord_x = int(input ("Dígame la coordenada X del barco a colocar:"))
+            coord_y = int(input ("Dígame la coordenada Y del barco a colocar:"))
             tablero[coord_x][coord_y] = "B"
             list_eslora = [1,2,3]
             orientacion = input ("Cómo quiere colocar el barco, en horizontal o en vertical?").lower()
@@ -87,8 +117,8 @@ def posicionar_barco (tablero):
                     for eslora in list_eslora:
                         tablero[coord_x + eslora][coord_y] = "B"
             num_portaaviones = num_portaaviones - 1
-            if num_portaaviones == 0:
-                print ("No quedan más portaaviones por colocar")
-                continue
-
+    
+    Numero_barcos = num_fragatas + num_destructores + num_acorazados + num_portaaviones
+    print (Numero_barcos,num_portaaviones,num_acorazados,num_destructores,num_fragatas)
+    
     return tablero
--- variables.py
+++ variables.py
@@ -3,4 +3,5 @@ num_fragatas = 4
 num_destructores = 3
 num_acorazados = 2
 num_portaaviones = 1
+
 Numero_barcos = num_fragatas + num_destructores + num_acorazados + num_portaaviones
